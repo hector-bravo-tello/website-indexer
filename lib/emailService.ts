f42e084884565d5ff24875014d2c6fec86a1bc92
Hector Bravo <hgbravo@gmail.com>
2024-10-24 14:39:20 -0500

    24-oct

--- lib/emailService.ts
+++ lib/emailService.ts
@@ -1,3 +1,4 @@
+import { createEmailNotification, getUserById } from '@/models';
 import nodemailer from 'nodemailer';
 import CONFIG from '@/config';
 
@@ -33,7 +34,7 @@ export async function sendEmail({ to, subject, html }: EmailOptions): Promise<vo
   }
 }
 
-export function generateIndexingEmail(domain: string, submittedUrls: string[]): string {
+function generateEmailBody(domain: string, submittedUrls: string[]): string {
   return `
     <h1>Indexing Report for ${domain}</h1>
     <p>The following URLs have been submitted for indexing:</p>
@@ -42,4 +43,36 @@ export function generateIndexingEmail(domain: string, submittedUrls: string[]): 
     </ul>
     <p>Total URLs submitted: ${submittedUrls.length}</p>
   `;
+}
+
+export async function sendEmailNotification(websiteId: number, userId: number, domain: string, type: 'job_complete' | 'job_failed', submittedUrls: string[]) {
+  try {
+    // generate the email body
+    const content = generateEmailBody(domain, submittedUrls);
+    
+    const { user } = await getUserById(userId);
+    if (user && user.email) {
+      // send the email
+      await sendEmail({
+        to: user.email,
+        subject: `Indexing ${type === 'job_complete' ? 'Completed' : 'Failed'}`,
+        html: content,
+      });
+      console.log(`Email notification sent to ${user.email} for website ${domain}`);
+
+    // add a record to the email_notifications table in database
+    await createEmailNotification({
+      user_id: userId,
+      website_id: websiteId,
+      type,
+      content
+    });
+
+    } else {
+      console.error(`User email not found for user ID ${userId}`);
+    }
+
+  } catch (error) {
+    console.error(`Error sending email notification for website ${domain}:`, error);
+  }
 }
--- lib/scheduledSitemapProcessor.ts
+++ lib/scheduledSitemapProcessor.ts
@@ -6,25 +6,47 @@ import {
   createIndexingJob,
   updateIndexingJob,
   createIndexingJobDetail,
-  updateWebsiteTimestamps,
-  createEmailNotification,
-  getUserById
+  updateWebsiteTimestamps
 } from '@/models';
 import { fetchBulkIndexingStatus, submitUrlForIndexing } from './googleSearchConsole';
-import { fetchUrl, extractSitemapUrls, parseSitemap, filterSitemaps, cleanDomain } from './sitemapProcessor';
-import { sendEmail, generateIndexingEmail } from '@/lib/emailService';
+import { fetchUrl, extractSitemapUrls, parseSitemap, filterSitemaps, cleanDomain, findAccessibleSitemap } from './sitemapProcessor';
+import { sendEmailNotification } from '@/lib/emailService';
+import { ValidationError } from '@/utils/errors';
 import { promisify } from 'util';
 
 const delay = promisify(setTimeout);
 const indexed: string = 'Submitted and indexed';
 
 export async function processWebsiteForScheduledJob(website: Website): Promise<void> {
+  const cleanedDomain = cleanDomain(website.domain);
+  let allSitemapUrls: string[] = [];
+
   try {
-    const cleanedDomain = cleanDomain(website.domain);
+    // First try to get sitemaps from robots.txt
     const robotsTxtUrl = `https://${cleanedDomain}/robots.txt`;
     const robotsTxtContent = await fetchUrl(robotsTxtUrl);
-    const allSitemapUrls = extractSitemapUrls(robotsTxtContent);
+    console.log('robotsTxtContent: ', robotsTxtContent);
+
+    allSitemapUrls = extractSitemapUrls(robotsTxtContent);
+    console.log('Error. allSitemapUrls: ', allSitemapUrls);
+
+  } catch(error) {
+    console.log('Failed to fetch robots.txt, trying to find sitemap directly... ', error);
+
+    // If robots.txt fails, try to find an accessible sitemap
+    const sitemapUrl = await findAccessibleSitemap(cleanedDomain);
+    allSitemapUrls = [sitemapUrl];
+    console.log('Error. allSitemapUrls: ', allSitemapUrls);
+  }
+
+  try {
+    // Filter and process the sitemaps
     const filteredSitemapUrls = filterSitemaps(allSitemapUrls);
+    console.log('filteredSitemapUrls: ', filteredSitemapUrls);
+
+    if (filteredSitemapUrls.length === 0) {
+      throw new ValidationError('No valid sitemaps found');
+    }
 
     // get all pages from sitemaps
     let allPages: Pick<Page, 'url'>[] = [];
@@ -33,16 +55,20 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
       const pages = await parseSitemap(sitemapContent);
       allPages = allPages.concat(pages);
     }
+    console.log('allPages: ', allPages);
 
     // create indexing job in database when starting
     const job = await createIndexingJob({ website_id: website.id, status: 'in_progress', total_pages: allPages.length });
+    console.log('job: ', job);
 
     // get websites pages from database
     const { pages: existingPages } = await getPagesByWebsiteId(website.id, true);
     const existingUrls = new Set(existingPages.map(page => page.url));
+    console.log('existingPages: ', existingPages);
 
     // website pages from sitemaps
     const currentUrls = new Set(allPages.map(page => page.url));
+    console.log('currentUrls: ', currentUrls);
 
     // get new, removed and unchanged urls
     const newUrls = allPages.filter(page => !existingUrls.has(page.url));
@@ -52,6 +78,7 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
     // get indexing data before submitting non-indexed pages
     const urlsToCheck = [...newUrls, ...unchangedUrls].map(page => page.url);
     let indexedPages = await fetchBulkIndexingStatus(website.id, urlsToCheck);
+    console.log('indexedPages: ', indexedPages);
 
     // Submit non-indexed pages
     let processedPages = 0;
@@ -60,13 +87,17 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
       if (page.indexingStatus !== indexed) {
         try {
           const response = await submitUrlForIndexing(website.domain, page.url);
+          console.log('response: ', response);
+
           await createIndexingJobDetail({
             indexing_job_id: job.job.id,
             page_id: existingPages.find(p => p.url === page.url)?.id || 0,
             status: 'Submitted',
-            response: JSON.stringify(response.data)
+            response: JSON.stringify(response)
           });
+          console.log('createIndexingJobDetail');
           processedPages++;
+
         } catch (error) {
           console.error(`Error submitting URL for indexing: ${page.url}`, error);
         }
@@ -78,6 +109,7 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
 
     // Fetch updated indexing statuses
     indexedPages = await fetchBulkIndexingStatus(website.id, urlsToCheck);
+    console.log('indexedPages: ', indexedPages);
 
     const pagesToUpdate = indexedPages.map(page => ({
       url: page.url,
@@ -85,6 +117,7 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
       lastCrawledDate: page.lastCrawledDate,
       lastSubmittedDate: page.indexingStatus !== indexed ? current_time : existingPages.find(p => p.url === page.url)?.last_submitted_date?.toISOString()
     }));
+    console.log('pagesToUpdate: ', pagesToUpdate);
 
     // Update database with final indexing data
     const result = await addOrUpdatePagesFromSitemap(website.id, pagesToUpdate);
@@ -97,17 +130,20 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
 
     // Update both last_sync and last_auto_index timestamps
     await updateWebsiteTimestamps(website.id, true, true);
+    console.log('updateWebsiteTimestamps');
 
     // update the the Indexing Job completion
     await updateIndexingJob(job.job.id, { 
       status: 'completed', 
       processed_pages: processedPages 
     });
+    console.log('updateIndexingJob');
 
     // Create email notification
-    const submittedUrls = pagesToUpdate.filter(page => page.indexingStatus === 'Submitted').map(page => page.url);
+    const submittedUrls = pagesToUpdate.filter(page => page.indexingStatus !== indexed).map(page => page.url);
+    console.log('submittedUrls: ', submittedUrls);
     if (submittedUrls.length > 0) {
-      await sendEmailNotification(website.id, website.user_id, website.domain, 'job_complete', submittedUrls);
+      await sendEmailNotification(website.id, website.user_id, cleanedDomain, 'job_complete', submittedUrls);
     }
 
   } catch (error) {
@@ -115,37 +151,5 @@ export async function processWebsiteForScheduledJob(website: Website): Promise<v
     // Send an email notification for failed indexing
     await sendEmailNotification(website.id, website.user_id, website.domain, 'job_failed', []);
     throw error;
-  }
-}
-
-async function sendEmailNotification(websiteId: number, userId: number, domain: string, type: 'job_complete' | 'job_failed', submittedUrls: string[]) {
-  try {
-    // generate the email body
-    const content = generateIndexingEmail(domain, submittedUrls);
-    
-    const { user } = await getUserById(userId);
-    if (user && user.email) {
-      // send the email
-      await sendEmail({
-        to: user.email,
-        subject: `Indexing ${type === 'job_complete' ? 'Completed' : 'Failed'} for ${domain}`,
-        html: content,
-      });
-      console.log(`Email notification sent to ${user.email} for website ${domain}`);
-
-    // add a record to the email_notifications table in database
-    await createEmailNotification({
-      user_id: userId,
-      website_id: websiteId,
-      type,
-      content
-    });
-
-    } else {
-      console.error(`User email not found for user ID ${userId}`);
-    }
-
-  } catch (error) {
-    console.error(`Error sending email notification for website ${domain}:`, error);
   }
 }
